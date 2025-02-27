cfcef1fb1f940dcf7d694f174e7d1e25097c078c
Tomasz Brengos <iwashis@gmail.com>
2025-02-27 20:49:43 +0100

    Reorganize lecture01

---
+++ Basics/src/Lecture01.hs
@@ -0,0 +1,25 @@
+module Lecture01 where 
+
+
+-- luzne kody z wykladu pierwszego
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) = quicksort le ++ [x] ++ quicksort gr
+  where
+    le = filter (< x) xs
+    gr = filter (>= x) xs
+
+add :: Int -> (Int -> Int)
+add x = \y -> x + y
+
+-- add x y =  x + y
+--
+t = add 6
+
+ones = 1 : ones
+
+fib = 0 : 1 : [x + y | (x, y) <- zip fib (drop 1 fib)] -- [0,1]
+
+sum' :: [Int] -> Int
+sum' [] = 0
+sum' (x : xs) = x + sum' xs
--- Basics/src/Lib.hs
+++ Basics/src/Lib.hs
@@ -5,24 +5,3 @@ module Lib (
 someFunc :: IO ()
 someFunc = putStrLn "someOtherFunc"
 
-quicksort :: (Ord a) => [a] -> [a]
-quicksort [] = []
-quicksort (x : xs) = quicksort le ++ [x] ++ quicksort gr
-  where
-    le = filter (< x) xs
-    gr = filter (>= x) xs
-
-add :: Int -> (Int -> Int)
-add x = \y -> x + y
-
--- add x y =  x + y
---
-t = add 6
-
-ones = 1 : ones
-
-fib = 0 : 1 : [x + y | (x, y) <- zip fib (drop 1 fib)] -- [0,1]
-
-sum' :: [Int] -> Int
-sum' [] = 0
-sum' (x : xs) = x + sum' xs
