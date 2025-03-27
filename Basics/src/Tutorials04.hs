f3712856078ece19fe286e8a97c69a3fa3b69078
Tomasz Brengos <iwashis@gmail.com>
2025-03-27 21:42:36 +0100

    Minor

--- Basics/src/Tutorials04.hs
+++ Basics/src/Tutorials04.hs
@@ -1,8 +1,6 @@
 module Tutorials04 where
 
 import qualified Data.Monoid as M 
-import Data.Foldable (Foldable(fold))
-import Control.Concurrent (yield)
 -- # Foldables
 --
 -- 1. **Implementacja map i filter za pomocą foldów**
@@ -45,8 +43,8 @@ data Tree a = Node a [Tree a]
 exampleTree :: Tree Int 
 exampleTree = Node 1 [ Node 2 [], Node 5 [Node 6 []]]
 
-exampleTreee :: Tree String 
-exampleTreee = Node "tomek" [ Node "ala" [], Node "maciek" [Node "ola" []]]
+exampleTree2 :: Tree String 
+exampleTree2 = Node "tomek" [ Node "ala" [], Node "maciek" [Node "ola" []]]
 
 
 instance Functor Tree where 
@@ -67,6 +65,9 @@ instance Foldable Tree where
 treeSum :: Num a => Tree a -> a
 treeSum tree = foldr (+) 0 tree
 
+
+-- teraz napiszemy to samo uzywajac foldMap i monoidu w uproszczonej wersji
+-- treeSum' :: Tree Int -> Int
 instance Semigroup Int where 
   x <> y = x + y 
 
@@ -76,23 +77,23 @@ instance Monoid Int where
 treeSum' :: Tree Int -> Int 
 treeSum' tree = foldMap id tree
 
-
+-- treeToList 
 treeToList' :: Tree a -> [a]
 treeToList' tree = foldMap return tree -- return = \x -> [x]
 
 -- 3. **Fold z kontrolą akumulacji**
 --
---    Zaimplementuj funkcję `foldlWithControl :: (b -> a -> Either b c) -> b -> [a] -> Either b c`, która
---    działa jak `foldl`, ale pozwala na przerwanie obliczenia w dowolnym momencie, zwracając aktualny akumulator
---    opakowany w `Left` lub finalny wynik w `Right`. Następnie użyj tej funkcji do implementacji:
---    - `findFirstThat :: (a -> Bool) -> [a] -> Maybe a` - znajduje pierwszy element spełniający warunek
---    - `takeWhileSum :: (Num a, Ord a) => a -> [a] -> [a]` - zwraca najdłuższy prefiks listy, którego suma nie przekracza podanej wartości
---    - `findSequence :: Eq a => [a] -> [a] -> Maybe Int` - znajduje indeks pierwszego wystąpienia podlisty w liście
---
+-- Zaimplementuj funkcję `foldlWithControl :: (b -> a -> Either b c) -> b -> [a] -> Either b c`, która
+-- działa jak `foldl`, ale pozwala na przerwanie obliczenia w dowolnym momencie, zwracając aktualny akumulator
+-- opakowany w `Left` lub finalny wynik w `Right`. Następnie użyj tej funkcji do implementacji:
+-- - `findFirstThat :: (a -> Bool) -> [a] -> Maybe a` - znajduje pierwszy element spełniający warunek
+-- - `takeWhileSum :: (Num a, Ord a) => a -> [a] -> [a]` - zwraca najdłuższy prefiks listy, którego suma nie przekracza podanej wartości
+-- - `findSequence :: Eq a => [a] -> [a] -> Maybe Int` - znajduje indeks pierwszego wystąpienia podlisty w liście
+
 
 -- data Either a b = Left a | Right b
 foldWithControl :: (b -> a -> Either b c) -> b -> [a] -> Either b c 
-foldWithControl f seed [] =  Left seed
+foldWithControl _ seed [] =  Left seed
 foldWithControl f seed (x:xs) = case f seed x of 
   Left b  -> foldWithControl f b xs 
   Right c -> Right c 
@@ -110,9 +111,7 @@ findFirstThat :: (a -> Bool) -> [a] -> Maybe a
 findFirstThat f list = toMaybe $ foldWithControl g () list 
   where 
     -- g :: () -> a -> Either () a
-    g () x = case f x of 
-      True  -> Right x
-      False -> Left () 
+    g () x = if f x  then Right x else Left ()
 
 takeWhileSum :: Int -> [Int] -> Either [Int] [Int]
 takeWhileSum max list = foldWithControl f [] list   -- takeWhileSum 10 [1,2,8,11] = Opakowane [1,2] 
@@ -122,32 +121,33 @@ takeWhileSum max list = foldWithControl f [] list   -- takeWhileSum 10 [1,2,8,11
 
 -- 4. **Odwracanie foldów**
 --
---    Zaimplementuj funkcję `unfoldl :: (b -> Maybe (b, a)) -> b -> [a]`, która jest odwrotnością `foldl` -
---    generuje listę z początkowego stanu. Użyj jej do implementacji:
+--    Zaimplementuj funkcję `unfoldl :: (b -> Maybe (b, a)) -> b -> [a]`, Użyj jej do implementacji:
 --    - `countdown :: Int -> [Int]` - generuje odliczanie od n do 1
 --    - `fib :: Int -> [Int]` - generuje n pierwszych liczb Fibonacciego
 --    - `iterate' :: (a -> a) -> a -> [a]` - własna implementacja standardowej funkcji `iterate`
 --    - `decToBin :: Int -> [Int]` - konwertuje liczbę dziesiętną na binarną reprezentację (listę 0 i 1)
 
 unfold :: (b -> Maybe (b,a)) -> b -> [a]
-unfold f state = go f state [] 
+unfold f s = go s [] 
   where 
-    go f state acc = case f state of 
+    go state acc = case f state of 
       Nothing -> acc
-      Just (state',obs) -> go f state' (acc ++ [obs]) 
+      Just (state',obs) -> go state' (acc ++ [obs]) 
 
 simpleAutomaton :: Char -> Maybe (Char, Int)
 simpleAutomaton 'x' = Just ('y',1)
 simpleAutomaton 'y' = Just ('z', 0)
 simpleAutomaton 'z' = Nothing
 simpleAutomaton _ = Nothing
-
+-- sprawdzic czym  jest unfold simpleAutomaton 'x'
 
 fib' :: Int -> [Int] 
 fib' n = unfold f state 
   where 
+    f :: (Int,Int,Int) -> Maybe ((Int,Int,Int),Int)
     f (_,_,0) = Nothing
     f (x,y,m) = Just ((y, x + y,m-1), x)  
+
     state = (0,1,n)
 
 -- 5. **Zaawansowana transformacja danych**  
