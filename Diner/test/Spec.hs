f46d20f91a1badd3bfb3a105942ed58e41db9e45
Tomasz Brengos <iwashis@gmail.com>
2025-05-14 09:29:48 +0200

    Change Spec

--- Diner/test/Spec.hs
+++ Diner/test/Spec.hs
@@ -1,15 +1,9 @@
 module Main (main) where
 
-import System.Environment (getArgs, withArgs)
+
 import Test.Framework (defaultMain)
-import qualified Parser.ParserSpec as P
+import qualified Parser.ParserSpec
 
 main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-        [] -> defaultMain allTests
-        _  -> withArgs args $ defaultMain allTests
+main = defaultMain Parser.ParserSpec.tests
 
--- Gather all tests from different test modules
-allTests = P.tests
